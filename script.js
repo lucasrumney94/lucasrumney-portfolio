548754063b136af166ce4a6c3b8e0675063a494f
Xueer Zhu <zhuxueerwhitney@gmail.com>
2016-12-03 14:19:06 -0500

    Update counter position and design

--- script.js
+++ script.js
@@ -5,9 +5,15 @@ window.onload = function() {
 	pr.onclick = slide.bind( this, -1 );
 	pl.onclick = slide.bind( this, 1 );
 	var index = 0, total = 5;
+
+	//temp
+	var placeholderText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."+
+							"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 	
+	placeholderText += placeholderText;
 	var title = [
 	"L U C A S  -  R U M N E Y",
+	"Bio",
 	"Hardware",
 	"Game Dev",
 	"Art",
@@ -16,11 +22,20 @@ window.onload = function() {
 	];
 
 	var content = [
-	"bio",
-	"Hardware stuff",
-	"game dev stuff",
-	"arty",
-	"music stuff"
+	"<div class=\"content\"><img src=\"lucas.png\" alt=\"\" width=\"100%\"></div>",
+	"<div class=\"content\"><p>bio</p>"+ placeholderText+ "</div>",
+	"<div class=\"content\"><p>Hardware</p>"+ placeholderText+ "</div>",
+	"<div class=\"content\"><p>game dev</p>"+ placeholderText+ "</div>",
+	"<div class=\"content\"><p>arty</p>"+ placeholderText+ "</div>",
+	"<div class=\"content\"><p>music</p>"+ placeholderText+ "</div>"
+	];
+
+	var bgColor = [
+	"",
+	"black",
+	"#178E96",
+	"#4F3A4B",
+	"#CE7182"
 	];
 
 
@@ -32,7 +47,8 @@ window.onload = function() {
 	  index = Math.min( Math.max( index + offset, 0 ), total - 1 );
 	  document.querySelector( '.counter' ).innerHTML = ( index + 1 ) + ' / ' + total;
 	  document.querySelector("#name").innerHTML = title[index];
-	  document.querySelector(".content").innerHTML = content[index];
+	  $("div.content").replaceWith(content[index]);
+	  $("div.content").css("background-color", bgColor[index]);
 
 
 	  pr.setAttribute( 'data-state', index === 0 ? 'disabled' : '' );
