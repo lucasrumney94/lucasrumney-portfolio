020cad1960c1cd1e0eb8a095ac4ece8ed345b792
Xueer Zhu <zhuxueerwhitney@gmail.com>
2016-12-03 15:18:26 -0500

    Add comments for lucas

--- script.js
+++ script.js
@@ -6,11 +6,31 @@ window.onload = function() {
 	pl.onclick = slide.bind( this, 1 );
 	var index = 0, total = 5;
 
-	//temp
+	//placeHolder Demonstration
 	var placeholderText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."+
 							"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-	
 	placeholderText += placeholderText;
+	//END
+
+	//TODO: you write html custom content here. REMEMBER to escape html ""
+	var bioHTML = "<p>for example, this is a bio p tag html</p><br><br><img alt=\"example image\">";
+	var hardwareHTML = "";
+	var gameHTML = "";
+	var artHTML = "";
+	var musicHTML= "";
+	//TODO-END
+
+	
+	//TODO: change title between strong tag to your title name
+	var content = [
+	"<div class=\"content\"><img src=\"lucas.png\" alt=\"\" width=\"100%\"></div>",
+	"<div class=\"content\"><strong>title</strong><br>"+ bioHTML+ "</div>",
+	"<div class=\"content\"><strong>title</strong><br>"+ hardwareHTML+ "</div>",
+	"<div class=\"content\"><strong>title</strong><br>"+ gameHTML+ "</div>",
+	"<div class=\"content\"><strong>title</strong><br>"+ artHTML+ "</div>",
+	"<div class=\"content\"><strong>title</strong><br>"+ musicHTML+ "</div>"
+	];
+
 	var title = [
 	"L U C A S  -  R U M N E Y",
 	"Bio",
@@ -19,15 +39,6 @@ window.onload = function() {
 	"Art",
 	"Music",
 	"Photography"
-	];
-
-	var content = [
-	"<div class=\"content\"><img src=\"lucas.png\" alt=\"\" width=\"100%\"></div>",
-	"<div class=\"content\"><p>bio</p>"+ placeholderText+ "</div>",
-	"<div class=\"content\"><p>Hardware</p>"+ placeholderText+ "</div>",
-	"<div class=\"content\"><p>game dev</p>"+ placeholderText+ "</div>",
-	"<div class=\"content\"><p>arty</p>"+ placeholderText+ "</div>",
-	"<div class=\"content\"><p>music</p>"+ placeholderText+ "</div>"
 	];
 
 	var bgColor = [
